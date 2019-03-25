76ccf26252e21b22500e82e575d1ad5001e3dfa3
Volodymyr Korniyko <v.korniyko@pm.me>
2019-03-25 17:08:02 +0200

    grunt, eslint, stylelint, htmlhint

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: "eslint.json"
+      },
+      target: ["gruntfile.js", "source/**/custom*.js"]
+    },
+
+    stylelint: {
+      options: {
+        configFile: ".stylelintrc",
+        formatter: "string",
+        ignoreDisables: false,
+        failOnError: true,
+        outputFile: "",
+        reportNeedlessDisables: false,
+        syntax: ""
+      },
+
+      all: ["source/**/*.css"]
+    },
+
+    htmlhint: {
+      options: {
+        htmlhintrc: ".htmlhintrc"
+      },
+      html1: {
+        src: ["source/**/*.html"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-eslint");
+  grunt.loadNpmTasks("grunt-stylelint");
+  grunt.loadNpmTasks("grunt-htmlhint");
+
+  grunt.registerTask("default", ["eslint", "stylelint", "htmlhint"]);
+};
